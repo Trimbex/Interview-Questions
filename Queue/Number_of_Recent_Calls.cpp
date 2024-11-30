8a44e69c6954200671fe596329188ad3cb2ebd83
Trimbex <111075379+Trimbex@users.noreply.github.com>
2024-11-30 04:45:37 +0200

    Update Number_of_Recent_Calls.cpp

--- Queue/Number_of_Recent_Calls.cpp
+++ Queue/Number_of_Recent_Calls.cpp
@@ -58,5 +58,5 @@ Constraints:
 
 1 <= t <= 10^9
 Each test case will call ping with strictly increasing values of t.
-At most 104 calls will be made to ping.
+At most 10^4 calls will be made to ping.
 */
